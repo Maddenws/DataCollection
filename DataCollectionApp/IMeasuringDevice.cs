0db67e13fc17db451c77d1a1c812809c9d7aa42a
Shane Madden <Madrox251@aol.com>
2021-10-14 22:31:18 -0400

    More Comments removed. IMeasuringDevice and MeasureingLengthDevice

--- DataCollectionApp/IMeasuringDevice.cs
+++ DataCollectionApp/IMeasuringDevice.cs
@@ -9,15 +9,15 @@ namespace DataCollectionApp
     //Interface for providing methods need to be used in MeasuringLengthDevice
     interface IMeasuringDevice
     {
-        //Will return the most recent measure in metric unit
+
         decimal MetricValue();
-        //Will return the most recent measure in imperial unit
+
         decimal ImperialValue();
-        //This will start the timers to collect measurements
+
         void StartCollecting();
-        //It will stop the tmers
+
         void StopCollecting();
-        //This method will return the most recent data from data array with their units and values
+
         int[] GetRawData();
     }
 }
